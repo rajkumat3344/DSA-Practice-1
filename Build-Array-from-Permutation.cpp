658aa6503116989567a46dcea8bf1d2b129ad2a8
RAJ KUMAR DUTTA <rajkumarsindri22@gmail.com>
2022-05-24 01:37:00 +0530

    Time Complexity attached

--- Build-Array-from-Permutation.cpp
+++ Build-Array-from-Permutation.cpp
@@ -5,11 +5,14 @@ using namespace std;
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); i++)
+        vector<int> ans; // T(1)
+        for (int i = 0; i < nums.size(); i++)//T(N)
         {
-            ans.push_back(nums[nums[i]]);
+            ans.push_back(nums[nums[i]]);//T(1)
         }
-        return ans;
+        return ans;//T(1)
     }
 };
+//TC = N + 1 + 1 + 1 = N + 3
+//We taking N by neglacting 3 [Time Complexity: O(N)]
+//Space Complexity: O(1)
