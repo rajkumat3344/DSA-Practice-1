80abdfc0fef103a4f5746a6385a0b92368ad4e38
RAJ KUMAR DUTTA <rajkumarsindri22@gmail.com>
2022-05-28 01:19:29 +0530

    day5-new solve

---
+++ Kth-smallest-element.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        
+    }
+};
