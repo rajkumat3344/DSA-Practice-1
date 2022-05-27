a9b8c159a7c8561800e4d2c28e49c3d26c0b09bf
RAJ KUMAR DUTTA <rajkumarsindri22@gmail.com>
2022-05-28 01:19:40 +0530

    day5

---
+++ Find-the-Frequency.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*Function to find frequency of x
+* x : element whose frequency is to be found
+* v : input vector
+*/
+int findFrequency(vector<int> v, int x){
+    // Your code here
+}
